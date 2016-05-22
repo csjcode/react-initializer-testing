556640cd206d90949c3015262aee80237ea4ed5c
csjcode <christophersstjohn@gmail.com>
2016-05-22 15:16:40 +0700

    Successfully ran first test

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # react-initializer-testing
-Testing demos with React, using the React Simple Starter server
+Testing demos with React & Mocha, using the React Simple Starter server
 
 npm run start
 http://localhost:8080/
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,8 @@
 import { renderComponent , expect } from '../test_helper'; // import test helper functions (needed for testing)
 import App from '../../src/components/app'; // import component being tested
 
+// to run this: npm run test:watch
+
 // 3 important keywords needed for the testing in this file - describe, it, expect
 
 /* Describe: groups together similar tests
@@ -9,12 +11,17 @@ import App from '../../src/components/app'; // import component being tested
 describe('App',() => {
 
    // It: Group one single test of target
-   // in this case we just want to test if the string "React Simple Starter" is rendered
+   // in this case we just want to test if the string "React simple starter" is rendered
 
    it('shows the correct text',() => {
 
+      // create an instance of App - renderComponent is one of the helpers already set up in test_helper
+      const component = renderComponent(App);
+
       // expect: make an 'assertion' (reasonable belief that belief about target is true) about the target
-      expect
-      
+      // .to.contain is a matcher
+      expect(component).to.contain('React simple starter')
+
    });
+
 });
