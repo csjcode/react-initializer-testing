8ec61f1f039560477891c31c49ee1e76dbacdbf1
csjcode <christophersstjohn@gmail.com>
2016-05-22 16:27:58 +0700

    Test component class name

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -1,7 +1,15 @@
 import { renderComponent , expect } from '../test_helper'; // import test helper functions (needed for testing)
 import CommentBox from '../../src/components/comment_box'; // import component being tested
 
+// selectors: https://github.com/chaijs/chai-jquery
+
 describe('CommentBox',() => {
+
+   it('has the class comment-box',() => {
+      const component = renderComponent(CommentBox);
+      expect(component).to.have.class('comment-box');
+   });
+
    it('has a text area',() => {
       const component = renderComponent(CommentBox);
       expect(component.find('textarea')).to.exist;
