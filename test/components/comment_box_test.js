162514f48c7099c1f675a10e482104f68e767301
csjcode <christophersstjohn@gmail.com>
2016-05-24 18:19:25 +0700

    2-14 - nested assertions for textarea in CommentBox

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -22,4 +22,22 @@ describe('CommentBox',() => {
       expect(component.find('button')).to.exist;
    });
 
+   describe('Entering some text',() => {
+
+      beforeEach(() => { // parent beforeEach fires for each it, then child forEach
+         component.find('textarea').simulate('change','new comment')
+      });
+
+      it('shows text in textarea',() => {
+         expect(component.find('textarea')).to.have.value('new comment');
+      });
+
+      it('when submitted clears the input',() => {
+         expect(component.find('button')).to.exist;
+      });
+
+
+   });
+
+
 });
