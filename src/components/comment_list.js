e466397e61b349a0056a0b3e377ddd53bf4b1917
csjcode <christophersstjohn@gmail.com>
2016-05-26 14:56:51 +0700

    2-18 Setup comment list, map array to li - no reducer yet

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const CommentList = (props) => {
+   const list = props.comments.map(comment => <li key={comment}>{comment}</li>)
    return (
       <ul className="comment-list">
-         <li>None</li>
+         {list}
       </ul>
    );
 };
 
-export default connect()(CommentList);
+function mapStateToProps(state){
+   return { comments: state.comments}
+}
+
+
+export default connect(mapStateToProps)(CommentList);
