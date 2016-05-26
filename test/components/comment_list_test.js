a6ccfaca18d5d8fc016da2a80c44a94a68e9014f
csjcode <christophersstjohn@gmail.com>
2016-05-26 14:47:32 +0700

    2-17 Build out CommentList tests

--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -4,6 +4,21 @@ import CommentBox from '../../src/components/comment_list'; // import component 
 
 describe('CommentList', () => {
 
+   let component;
+
+   beforeEach(() => {
+      const props =  {comments:['Thailand','Malaysia']};
+      component = renderComponent(CommentList, null, props); // 3rd argument can pass props down to component
+   });
+
+   it('shows an li for each comment', () => {
+      expect(component.find('li').length).to.equal(2);
+   });
+
+   it('shows each comment that is provided', () => {
+      expect(component).to.contain("Thailand");
+      expect(component).to.contain("Malaysia");
+   });
 
 
 });
