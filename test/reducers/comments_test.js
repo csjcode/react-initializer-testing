4979e8a04961fab13f24a13b3a09392ba91200f3
csjcode <christophersstjohn@gmail.com>
2016-05-26 17:34:39 +0700

    2-22 fixed comment reducer test error

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -5,7 +5,7 @@ import {SAVE_COMMENT} from '../../src/actions/types';
 describe('Comments Reducer', () => {
    it('Handles action with unknown type', () => {
       // expect(commentReducer()).to.be.instanceof(Array);
-      expect(commentReducer()).to.eql([]);
+      expect(commentReducer(undefined,{})).to.eql([]);
    });
 
    it('Handles action of type SAVE_COMMENT', () => {
