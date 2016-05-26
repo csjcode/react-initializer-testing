604354d53ee17fbbd97f83ce1f0d72b3def2f08f
csjcode <christophersstjohn@gmail.com>
2016-05-26 16:45:09 +0700

    2-19 Scaffold out actions

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+import { SAVE_COMMENT } from './types';
+
+
+export function saveComment() {
+
+}
---
+++ test/actions/index_test.js
@@ -0,0 +1,15 @@
+import { expect } from '../test_helper'; // import test helper functions (needed for testing)
+
+describe('actions', () => {
+   describe('saveComment', () => {
+      it('it has the correct type',() => {
+
+      });
+
+      it('it has the correct payload',() => {
+
+      });
+
+   });
+
+});
