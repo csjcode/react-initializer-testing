c48ce9dfa7c70722e5f9e5f7fdae53298c977aeb
csjcode <christophersstjohn@gmail.com>
2016-05-26 15:06:21 +0700

    2-18 Modified reducer & fixed CommentList vs. CommentBox typo

--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -1,5 +1,5 @@
 import { renderComponent , expect } from '../test_helper'; // import test helper functions (needed for testing)
-import CommentBox from '../../src/components/comment_list'; // import component being tested
+import CommentList from '../../src/components/comment_list'; // import component being tested
 
 
 describe('CommentList', () => {
