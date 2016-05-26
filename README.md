343a3fd52fa1d654aa4a67c8b26c54e8128f9738
csjcode <christophersstjohn@gmail.com>
2016-05-26 14:27:45 +0700

    Added CommentList assertion spec to App

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # react-initializer-testing
 Testing demos with React & Mocha, using the React Simple Starter server
 
+npm run test:watch 
 npm run start
 http://localhost:8080/
---
+++ src/components/comment_list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommentList = (props) => {
+   return (
+      <ul className="comment-list">
+         <li>None</li>
+      </ul>
+   );
+};
+
+export default CommentList
