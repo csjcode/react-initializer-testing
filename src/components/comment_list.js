2955cf3711e6170f276d04b4edea62249bf8f64e
csjcode <christophersstjohn@gmail.com>
2016-05-26 15:08:09 +0700

    2-18 Explanation of why mapStateToProps function

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -4,13 +4,11 @@ import { connect } from 'react-redux';
 const CommentList = (props) => {
    const list = props.comments.map(comment => <li key={comment}>{comment}</li>)
    return (
-      <ul className="comment-list">
-         {list}
-      </ul>
+      <ul className="comment-list">{list}</ul>
    );
 };
 
-function mapStateToProps(state){
+function mapStateToProps(state){ // this is done to to change CommentList to container for Redux
    return { comments: state.comments}
 }
 
