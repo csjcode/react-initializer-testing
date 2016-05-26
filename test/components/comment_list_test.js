613d449f82c5907e347f3fb0decdd8192b73fd90
csjcode <christophersstjohn@gmail.com>
2016-05-26 14:31:53 +0700

    2-16 stub out CommentList test spec

---
+++ test/components/comment_list_test.js
@@ -0,0 +1,9 @@
+import { renderComponent , expect } from '../test_helper'; // import test helper functions (needed for testing)
+import CommentBox from '../../src/components/comment_list'; // import component being tested
+
+
+describe('CommentList', () => {
+
+
+
+});
