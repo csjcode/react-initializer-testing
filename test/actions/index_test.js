9022daaedff3b01770fe66ea46fea66d731b3720
csjcode <christophersstjohn@gmail.com>
2016-05-26 16:52:22 +0700

    2-19 Basic action scaffolding done - move to implementation

--- test/actions/index_test.js
+++ test/actions/index_test.js
@@ -5,10 +5,13 @@ import { save_comment } from '../../src/actions';
 describe('actions', () => {
    describe('saveComment', () => {
       it('it has the correct type',() => {
-
+         const action = saveComment();
+         expect(action.type).to.equal(SAVE_COMMENT);
       });
 
       it('it has the correct payload',() => {
+         const action = saveComment('new comment');
+         expect(action.payload).to.equal('new comment');
 
       });
 
